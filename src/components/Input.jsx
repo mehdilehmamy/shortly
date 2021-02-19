a60739741a9874e17c561b85b3d29a5fe9bdc96d
mehnou <mehnou@gmail.com>
2021-02-19 18:07:41 +0100

    Add link functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const API = "https://api.shrtco.de/v2/shorten?url="; //ShortURL API endpoint
+
+
 
 export default function Input () {
+    const [inputURL, setInputURL] = useState("");
+    const [urlList, setUrlList] = useState([]);
+
+    function handleChange (e) {
+        let userInput = e.target.value;
+        setInputURL(userInput);
+    }
+
+    function handleClick () {
+        axios.get(`${API}${inputURL}`)
+        .then(function (response) {
+            // handle success
+            setUrlList((prev) => {
+                return [...prev, response.data]
+            });
+        console.log(urlList);
+         }).catch(function (error) {
+            // handle error
+            console.log("Something went wrong: " + error);
+          });
+        setInputURL("");
+    }
     return (
-        <div className="url-input" id="shorter">
-            <input type="text" placeholder="Shorten a link here..."/>
-            <button>Shorten It!</button>
-        </div>
+       <div>
+           <div className="url-input" id="shorter">
+                <input type="text" placeholder="Shorten a link here..." onChange={handleChange} value={inputURL}/>
+                <button onClick={handleClick}>Shorten It!</button>
+            </div>
+            <div className="url-list">
+             {urlList.map((url) => {
+                    return <div className="url">
+                             <p>{url.result.original_link}</p>
+                             <a href={"http://" + url.result.short_link} target="_blank">{url.result.short_link}</a>
+                           </div>
+                })}
+            </div>
+       </div>
     );
 }
