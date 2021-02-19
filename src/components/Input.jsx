71e834c8d717f7c211cd6e9287a1c287a7a130f7
mehnou <mehnou@gmail.com>
2021-02-19 19:51:58 +0100

    Add copy functionality

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,6 +8,7 @@ const API = "https://api.shrtco.de/v2/shorten?url="; //ShortURL API endpoint
 export default function Input () {
     const [inputURL, setInputURL] = useState("");
     const [urlList, setUrlList] = useState([]);
+    
 
     function handleChange (e) {
         let userInput = e.target.value;
@@ -21,7 +22,6 @@ export default function Input () {
             setUrlList((prev) => {
                 return [...prev, response.data]
             });
-        console.log(urlList);
          }).catch(function (error) {
             // handle error
             console.log("Something went wrong: " + error);
@@ -36,10 +36,16 @@ export default function Input () {
             </div>
             <div className="url-list">
              {urlList.map((url) => {
-                    return <div className="url">
+                    return (<div className="myurl">
+                        <div className="url" key={Math.random()}>
                              <p>{url.result.original_link}</p>
                              <a href={"http://" + url.result.short_link} target="_blank">{url.result.short_link}</a>
-                           </div>
+                        </div>
+                        <button className="copy" onClick={() => {
+                                navigator.clipboard.writeText(url.result.short_link);
+                                window.alert("Text copied!");
+                                }}>Copy Link</button>
+                    </div>);
                 })}
             </div>
        </div>
